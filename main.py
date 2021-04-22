5b441c96e42d60a0e564af323706751409bb16e3
David Beradze <daviti.beradze.1@btu.edu.ge>
2021-04-22 22:37:41 +0400

    first commit

--- main.py
+++ main.py
@@ -1,49 +1,59 @@
+import sqlite3
+
+connection = sqlite3.connect("dataset2.db")
+
+cursor = connection.cursor()
+
+# cursor.execute("CREATE TABLE dataset2('OrderDate' TEXT, 'Region' TEXT, 'Client' TEXT, 'Item' TEXT, 'Units' INTEGER, 'UnitCost' REAL, 'Total' REAL)")
+
+
+# a = open("dataset2.csv", "r")
+#
+# for i in range(43):
+#     temp = ""
+#     b = a.readline()
+#     b = b.split(",")
+#     cursor.execute(f"INSERT INTO dataset2 VALUES('{b[0]}', '{b[1]}', '{b[2]}', '{b[3]}', '{b[4]}', '{b[5]}', '{b[6]}')")
+# a.close()
+
+
+
 # task 1
 
-# import requests
-#
-# response = requests.get("https://www.mes.gov.ge/index.php?lang=geo")
-#
-# a = open("mes.gov.ge", "w")
-# a.write(str(response.content))
-#
-# print(str(response.content).count("განათლება"))
+# cursor.execute("SELECT SUM(Total) FROM dataset2 WHERE REGION = 'Central'")
+# print(cursor.fetchall())
+# cursor.execute("SELECT SUM(Total) FROM dataset2 WHERE REGION = 'East'")
+# print(cursor.fetchall())
+# cursor.execute("SELECT SUM(Total) FROM dataset2 WHERE REGION = 'West'")
+# print(cursor.fetchall())
 
 
 
 # task 2
 
-# import requests
-#
-# response1 = requests.get("https://httpbin.org/image/jpeg")
-# a = open("image1.png", "wb")
-# a.write(response1.content)
-# a.close()
-#
-# response2 = requests.get("https://httpbin.org/image/png")
-# b = open("image2.png", "wb")
-# b.write(response2.content)
-# b.close()
-#
-# response3 = requests.get("https://httpbin.org/image/svg")
-# c = open("image3.png", "wb")
-# c.write(response3.content)
-# c.close()
-#
-# response4 = requests.get("https://httpbin.org/image/webp")
-# d = open("image4.png", "wb")
-# d.write(response4.content)
-# d.close()
+# cursor.execute("SELECT Client FROM dataset2 WHERE Total = MAX(Total)")
+# print(cursor.fetchall())
 
 
 
 # task 3
 
-import requests
+# cursor.execute("SELECT Product FROM dataset2 WHERE Total = MAX(Total)")
+# print(cursor.fetchall())
 
-response = requests.get("https://httpbin.org/ip")
 
-a = open("my-ip.txt", "w")
-for i in str(response.content):
-    if i.isnumeric() or i == ".":
-        a.write(str(i))
+
+# task 4
+
+# cursor.execute("SELECT AVG(UnitCost) FROM dataset2")
+# print(cursor.fetchall())
+
+
+
+# task 5
+
+cursor.execute("SELECT SUM(Total) FROM dataset2")
+print(cursor.fetchall())
+
+connection.commit()
+
